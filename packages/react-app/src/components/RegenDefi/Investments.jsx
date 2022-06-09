258d14e280d67d65becf94326de572e01898005d
Guillermo Acuña <gjacuna@gmail.com>
2022-06-09 09:59:28 -0400

    balance per date

--- packages/react-app/src/components/RegenDefi/Investments.jsx
+++ packages/react-app/src/components/RegenDefi/Investments.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 import React, { useContext, useEffect, useState } from 'react'
 import { Line } from 'react-chartjs-2'
 import { Card, Col, Image, List, Row, Typography } from 'antd'
@@ -39,6 +40,23 @@ const Investments = ({ address, contracts }) => {
   const cbtcTxs = useOldBalance(address,'0x7958e9fa5cf56aebedd820df4299e733f7e8e5dd')
   const cnbedTxs = useOldBalance(address,'0x0765425b334d7db1f374d03f4261ac191172bef7')
 
+  // const nctPrice = usePriceHistory('0xD838290e877E0188a4A44700463419ED96c16107',30,'daily')
+  // const bctPrice = usePriceHistory('0x2F800Db0fdb5223b3C3f354886d907A671414A7F',30,'daily')
+  // const mco2Price = usePriceHistory('0xAa7DbD1598251f856C12f63557A4C4397c253Cea',30,'daily')
+  // const klimaPrice = usePriceHistory('0x4e78011ce80ee02d2c3e649fb657e45898257815',30,'daily')
+  // const sklimaPrice = usePriceHistory('0xb0C22d8D350C67420f06F48936654f567C73E8C8',30,'daily')
+
+  const wethAddress = '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619'
+  const wbtcAddress = '0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6'
+  const dfiAddress = '0x85955046DF4668e1DD369D2DE9f3AEB98DD2A369'
+  const priceHistory = []
+
+  priceHistory[wethAddress] = usePriceHistory(wethAddress,30,'daily')
+  priceHistory[wbtcAddress] = usePriceHistory(wbtcAddress,30,'daily')
+  priceHistory[dfiAddress] = usePriceHistory(dfiAddress,30,'daily')
+
+  // console.log(nctPrice)
+
   const getDatesBetween = pastDays => {
     const dates = []
     const endDate = new Date()
@@ -61,12 +79,49 @@ const Investments = ({ address, contracts }) => {
 
       if(nctTxs && bctTxs && mco2txs && klimaTxs && sklimaTxs && cbtcTxs && cnbedTxs) {
         const nctBalance = {}
+        const bctBalance = {}
+        const mco2Balance = {}
+        const klimaBalance = {}
+        const sklimaBalance = {}
+        const cbtcBalance = {}
+        const cnbedBalance = {}
 
-        // console.log(nctTxs)
         dates.forEach(date => {
+          nctBalance[date.getTime()] = 0
+          bctBalance[date.getTime()] = 0
+          mco2Balance[date.getTime()] = 0
+          klimaBalance[date.getTime()] = 0
+          sklimaBalance[date.getTime()] = 0
+          cbtcBalance[date.getTime()] = 0
+          cnbedBalance[date.getTime()] = 0
+
           nctTxs.forEach(tx => {
-            if(tx.timeStamp <= date.getTime())
-              nctBalance[date.getTime()] += (address.toLowerCase() === tx.from.toLowerCase() ? (1) : (-1)) * Number(utils.formatUnits(tx.value,tx.tokenDecimal))
+            if(tx.timeStamp*1000 <= date.getTime())
+              nctBalance[date.getTime()] += (address.toLowerCase() === tx.from.toLowerCase() ? (-1) : (1)) * Number(utils.formatUnits(tx.value,tx.tokenDecimal))
+          })
+          bctTxs.forEach(tx => {
+            if(tx.timeStamp*1000 <= date.getTime())
+              bctBalance[date.getTime()] += (address.toLowerCase() === tx.from.toLowerCase() ? (-1) : (1)) * Number(utils.formatUnits(tx.value,tx.tokenDecimal))
+          })
+          mco2txs.forEach(tx => {
+            if(tx.timeStamp*1000 <= date.getTime())
+              mco2Balance[date.getTime()] += (address.toLowerCase() === tx.from.toLowerCase() ? (-1) : (1)) * Number(utils.formatUnits(tx.value,tx.tokenDecimal))
+          })
+          klimaTxs.forEach(tx => {
+            if(tx.timeStamp*1000 <= date.getTime())
+              klimaBalance[date.getTime()] += (address.toLowerCase() === tx.from.toLowerCase() ? (-1) : (1)) * Number(utils.formatUnits(tx.value,tx.tokenDecimal))
+          })
+          sklimaTxs.forEach(tx => {
+            if(tx.timeStamp*1000 <= date.getTime())
+              sklimaBalance[date.getTime()] += (address.toLowerCase() === tx.from.toLowerCase() ? (-1) : (1)) * Number(utils.formatUnits(tx.value,tx.tokenDecimal))
+          })
+          cbtcTxs.forEach(tx => {
+            if(tx.timeStamp*1000 <= date.getTime())
+              cbtcBalance[date.getTime()] += (address.toLowerCase() === tx.from.toLowerCase() ? (-1) : (1)) * Number(utils.formatUnits(tx.value,tx.tokenDecimal))
+          })
+          cnbedTxs.forEach(tx => {
+            if(tx.timeStamp*1000 <= date.getTime())
+              cnbedBalance[date.getTime()] += (address.toLowerCase() === tx.from.toLowerCase() ? (-1) : (1)) * Number(utils.formatUnits(tx.value,tx.tokenDecimal))
           })
         })
         console.log(nctBalance)
